9edd20bad402ac9c9f6064601a6a07258c036ead
Benjamin Dulau <benjamin.dulau@gmail.com>
2013-04-17 09:26:14 +0200

    Initial bundle structure

---
+++ KitanoPelBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of KitanoPelBundle
+ *
+ * (c) Kitano <contact@kitanolabs.org>
+ *
+ */
+
+namespace Kitano\PelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class KitanoPelBundle
+ *
+ * @author Benjamin Dulau <benjamin.dulau@gmail.com>
+ */
+class KitanoPelBundle extends Bundle
+{
+}
---
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of KitanoPelBundle
+ *
+ * (c) Kitano <contact@kitanolabs.org>
+ *
+ */
+
+namespace Kitano\PelBundle\Tests\DependencyInjection;
+
+/**
+ * Class ConfigurationTest
+ *
+ * @author Benjamin Dulau <benjamin.dulau@gmail.com>
+ */
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
